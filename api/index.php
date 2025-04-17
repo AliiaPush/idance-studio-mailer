b16ea0f36c1d6daf3d4677526654fd08692a103f
Luke Fernando <isanjaya139@gmail.com>
2025-04-17 20:52:13 +0530

    added a debugging statement

--- api/index.php
+++ api/index.php
@@ -5,15 +5,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+if (file_exists(__DIR__ . '/../.env')) {
+    echo "Exists";
+}
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $mail = new PHPMailer(true);
 
-if (file_exists(__DIR__ . '/../.env')) {
-    echo "Exists";
-}
 
 echo "Hello Vercel";
 echo $_ENV['SAMPLE_API_KEY'];
