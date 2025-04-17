cfa55db0ab9dd173f4ada29f7090a7de12d7ba82
Luke Fernando <isanjaya139@gmail.com>
2025-04-17 20:30:31 +0530

    autoloader require statement moved to the very top of the script

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
