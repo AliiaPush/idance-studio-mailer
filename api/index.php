e4a7537416ed133ab2c9abd5ced47f6d04d62bfa
Luke Fernando <85795256+Luke-Fernando@users.noreply.github.com>
2025-08-17 07:28:37 +0530

    sentry logs added

--- api/index.php
+++ api/index.php
@@ -10,6 +10,9 @@ if (file_exists(__DIR__ . '/../.env')) {
     $dotenv->load();
 }
 
+\Sentry\init([
+    'dsn' => $_ENV["SENTRY_DSN"],
+]);
 
 header("Access-Control-Allow-Origin: *");
 
@@ -51,100 +54,14 @@ function set_SMTP($name, $subject, $template)
         echo 'success';
     } catch (Exception $ex) {
         echo "Message could not be sent.";
+        \Sentry\captureException($ex);
+        if (isset($mail)) {
+            \Sentry\captureMessage("PHPMailer Error: " . $mail->ErrorInfo);
+        }
     }
 }
 
-// if (isset($_POST["execution"]) && $_POST["execution"] == "contact") {
-//     if (isset($_POST["name"]) && !empty($_POST["name"])) {
-//         if (isset($_POST["topic"]) && $_POST["topic"] != "0" && !empty($_POST["topic"])) {
-//             if (isset($_POST["email"]) && !empty($_POST["email"])) {
-//                 //
-//                 if (isset($_POST["subject"]) && !empty($_POST["subject"])) {
-//                     if (isset($_POST["message"]) && !empty($_POST["message"])) {
-//                         $name = $_POST["name"];
-//                         if ($_POST["topic"] == 1) {
-//                             $topic = "Anmeldung für Probestunde";
-//                         } else if ($_POST["topic"] == 2) {
-//                             $topic = "Fragen zu Kursen und Zeiten";
-//                         } else if ($_POST["topic"] == 3) {
-//                             $topic = "Anfrage für private Tanzstunden";
-//                         } else if ($_POST["topic"] == 4) {
-//                             $topic = "Feedback geben";
-//                         } else if ($_POST["topic"] == 5) {
-//                             $topic = "Anderes";
-//                         }
-//                         $email = $_POST["email"];
-//                         $subject = $_POST["subject"];
-//                         $message = $_POST["message"];
-//                         ob_start();
-//                         require_once "contact.php";
-//                         $template = ob_get_clean();
-//                         set_SMTP($name, "Contacting Idance Studio", $template);
-//                     } else {
-//                         echo ("please add your message");
-//                     }
-//                 } else {
-//                     echo ("please add a subject");
-//                 }
-//                 //
-//             } else {
-//                 echo ("you must add your email to continue");
-//             }
-//         } else {
-//             echo ("please select a topic");
-//         }
-//     } else {
-//         echo ("you must add your name to continue");
-//     }
-// }
 
-
-// if (isset($_POST["execution"]) && $_POST["execution"] == "register") {
-//     if (isset($_POST["first_name"]) && !empty($_POST["first_name"])) {
-//         if (isset($_POST["last_name"]) && !empty($_POST["last_name"])) {
-//             if (isset($_POST["class_category"]) && !empty($_POST["class_category"]) && $_POST["class_category"] != "0") {
-//                 if (isset($_POST["mobile_number"]) && !empty($_POST["mobile_number"])) {
-//                     if (isset($_POST["email"]) && !empty($_POST["mobile_number"])) {
-//                         $first_name = $_POST["first_name"];
-//                         $last_name = $_POST["last_name"];
-//                         $class_category = "";
-//                         if ($_POST["class_category"] == 1) {
-//                             $class_category = "Minis";
-//                         } else if ($_POST["class_category"] == 2) {
-//                             $class_category = "Kids";
-//                         } else if ($_POST["class_category"] == 3) {
-//                             $class_category = "Teens";
-//                         } else if ($_POST["class_category"] == 4) {
-//                             $class_category = "Custom";
-//                         }
-//                         $mobile_number = $_POST["mobile_number"];
-//                         $email = $_POST["email"];
-//                         $message = "";
-//                         if (isset($_POST["message"]) && !empty($_POST["message"])) {
-//                             $message = $_POST["message"];
-//                         } else {
-//                             $message = "Message is empty!";
-//                         }
-//                         ob_start();
-//                         require_once "register.php";
-//                         $template = ob_get_clean();
-//                         set_SMTP("$first_name $last_name", "Class Registration", $template);
-//                     } else {
-//                         echo ("please add a email to continue");
-//                     }
-//                 } else {
-//                     echo ("please add a mobile number to continue");
-//                 }
-//             } else {
-//                 echo ("please add your first name to continue");
-//             }
-//         } else {
-//             echo ("please add your last name to continue");
-//         }
-//     } else {
-//         echo ("please add your first name to continue");
-//     }
-// }
 
 $contact = [
     [
